d43a84331a18e1eb99401b0a76f1699dca928cf2
Pulse-External <321live.on.yt@gmail.com>
2025-11-23 20:07:38 +0100

    Create Inject.js

---
+++ Inject.js
@@ -0,0 +1,2 @@
+fetch('https://raw.githubusercontent.com/nostopgmaming17/scratchSploit/refs/heads/main/api.js').then(r => r.text()).then(eval).c // API by nostopgmaming17
+fetch('https://raw.githubusercontent.com/Pulse-External/Scratch-External-ESP/refs/heads/main/External-ESP-PulsExternal.js').then(r => r.text()).then(eval).c // External ESP
